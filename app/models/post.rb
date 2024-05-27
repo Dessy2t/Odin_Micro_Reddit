3b351cdde9e7b9671b12df319c1918afa997a6a6
Desmond Yentumi <yentumid7@gmail.com>
2024-05-27 21:00:00 +0000

    added a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
-class Post < ApplicationRecord
+class CreatePosts < ActiveRecord::Migration[6.0]
+    def change
+      create_table :posts do |t|
+        t.string :title
+        t.text :body
+  
+        t.timestamps
+       end
+    end
 end
+  
+class Post < ApplicationRecord
+    validates :title, presence: true, length: { maximum: 45 }
+    validates :body, presence: true, length: { maximum: 200 }
+end
+
