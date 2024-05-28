2b467f9c303808ed37ea9d341afb49dd005554f2
Desmond Yentumi <yentumid7@gmail.com>
2024-05-28 11:33:16 +0000

    new comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :posts
   validates :body, presence, true
+  balidates :post, presence, true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -13,4 +13,5 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: { maximum: 45 }
     validates :body, presence: true, length: { maximum: 200 }
     belongs_to :user
+    has_many :comments
 end
