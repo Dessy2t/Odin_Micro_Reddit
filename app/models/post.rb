5634b7b9d48263046e870b9e61e1acf7fa677a74
Desmond Yentumi <yentumid7@gmail.com>
2024-05-28 11:10:44 +0000

    added a coment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,5 +12,5 @@ end
 class Post < ApplicationRecord
     validates :title, presence: true, length: { maximum: 45 }
     validates :body, presence: true, length: { maximum: 200 }
+    belongs_to :user
 end
-
